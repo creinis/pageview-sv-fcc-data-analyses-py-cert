00ee42729f16619191190e559b0bbf3df571b588
creinis <115181527+creinis@users.noreply.github.com>
2024-08-05 19:01:11 +0200

    feat: The title should be Daily freeCodeCamp Forum Page Views 5/2016-12/2019. The label on the x axis should be Date and the label on the y axis should be Page Views.

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -15,6 +15,12 @@ df = df[(df['value'] >= df['value'].quantile(0.025))
         & (df['value'] <= df['value'].quantile(0.975))]
 
 def draw_line_plot():
+    # Draw line plot
+    fig, ax = plt.subplots(figsize=(14, 7))
+    ax.plot(df.index, df['value'], color='red', linewidth=1)
+    ax.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Page Views')
     
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
