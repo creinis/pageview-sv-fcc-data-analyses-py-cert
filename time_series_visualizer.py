46f7bc90569977dc26442cd47b5715c7f5e1bc6a
creinis <115181527+creinis@users.noreply.github.com>
2024-08-05 18:56:27 +0200

    feat: importa data frame: Use Pandas to import the data from "fcc-forum-pageviews.csv". Set the index to the date column.

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,5 +5,10 @@ from pandas.plotting import register_matplotlib_converters
 
 register_matplotlib_converters()
 
+# Import data (Make sure to parse dates. Consider setting index column to 'date'.)
+df = pd.read_csv('fcc-forum-pageviews.csv',
+                 parse_dates=['date'],
+                 index_col='date')
+
 
 
