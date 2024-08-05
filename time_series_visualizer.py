8bfc9f79689ceefd127e3f50774bc41ab72062d8
creinis <115181527+creinis@users.noreply.github.com>
2024-08-05 18:56:59 +0200

    feat: Clean the data by filtering out days when the page views were in the top 2.5% of the dataset or bottom 2.5% of the dataset.

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -10,5 +10,9 @@ df = pd.read_csv('fcc-forum-pageviews.csv',
                  parse_dates=['date'],
                  index_col='date')
 
+# Clean data
+df = df[(df['value'] >= df['value'].quantile(0.025))
+        & (df['value'] <= df['value'].quantile(0.975))]
+
 
 
